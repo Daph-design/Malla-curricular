641a4167cbf61ed700ef6225efa517d6219c30a1
Daph-design <daphnejfd@gmail.com>
2025-07-13 14:30:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 // Ejemplo de cursos
 const courses = [
-  { id: "MAT101", name: "Cálculo I", prerequisites: [] },
-  { id: "MAT201", name: "Cálculo II", prerequisites: ["MAT101"] },
-  { id: "FIS101", name: "Física I", prerequisites: [] },
-  { id: "FIS201", name: "Física II", prerequisites: ["FIS101"] },
+  { id: "CC1101", name: "Cálculo de una Variable", prerequisites: [] },
+  { id: "BI1001", name: "Introducción a la Bioingeniería", prerequisites: [] },
+  { id: "CS1111", name: "Programación I", prerequisites: [] },
+  { id: "PI1101", name: "Proyectos Interdisciplinarios 1", prerequisites: [] },
+  { id: "HH1101", name: "Laboratorio de Comunicación 1", prerequisites: [] },
+  { id: "CC1142", name: "Laboratorio de Química General", prerequisites: [] },
+  { id: "CC1141", name: "Química General", prerequisites: [] },
+  { id: "CC1103", name: "Álgebra lineal", prerequisites: ["CC1101"] },
+  { id: "BI1002", name: "Introducción a la Bioingeniería", prerequisites: ["BI1001"] },
+  { id: "CC1104", name: "Cálculo vectorial", prerequisites: ["CC1101"] },
+  { id: "CC1121", name: "Introducción a la Mecánica", prerequisites: ["CC1101"] },
+  { id: "HH1102", name: "Laboratorio de Comunicación 2", prerequisites: ["HH1101"] },
+  { id: "BI1003", name: "Química Aplicada", prerequisites: ["CC1141", "CC1142"] },
 ];
 
 // Recupera cursos aprobados del almacenamiento local
